20a6aef505146bef3c8852997e8b86f436ba840d
Sightless21 <pou.phanupong@gmail.com>
2026-07-09 13:54:34 +0700

    Update role

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,14 +3,13 @@ import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { CurrentUser } from '@/auth/decorator/current-user.decorator';
 import { Roles } from '@/auth/decorator/roles.decorator';
-import { Role } from '@prisma/client';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  @Roles(Role.STUDENT)
+  @Roles('STUDENT')
   create(@Body() data: CreateOrderDto, @CurrentUser('sub') userId: string) {
     return this.ordersService.create(userId, data);
   }
